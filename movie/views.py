ca843bbfd6caea5e0fbb0670fff55764032b03e3
Juan José Sanchez <jjsanchezc@eafit.edu.co>
2022-02-10 18:20:05 -0500

    home.html

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html',{'name':'greg'})
+    return render(request,'home.html',{'name':'juan jose',"dia":datetime.now()})
+
+def segundapantalla(request):
+    return HttpResponse("esta es la segunda pantallaaa")
 #pre incubacion 
